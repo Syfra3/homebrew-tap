3c455d61bae797d15191da7a8c2c950a2dd50291
Matias Celiz <matias.celizm@gmail.com>
2026-04-05 21:31:32 -0500

    Update formula to use public Syfra3/ancora repository
    - Change homepage to github.com/Syfra3/ancora
    - Update release URLs from ancora-vX.Y.Z to vX.Y.Z format
    - Formula now points to open source repository

--- Formula/ancora.rb
+++ Formula/ancora.rb
@@ -1,25 +1,25 @@
 class Ancora < Formula
   desc "Persistent memory system for AI agents with hybrid search and MCP integration"
-  homepage "https://github.com/Syfra3/syfra"
+  homepage "https://github.com/Syfra3/ancora"
   version "1.0.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Syfra3/syfra/releases/download/ancora-v#{version}/ancora-#{version}-darwin-arm64.tar.gz"
+      url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-darwin-arm64.tar.gz"
       sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
     else
-      url "https://github.com/Syfra3/syfra/releases/download/ancora-v#{version}/ancora-#{version}-darwin-amd64.tar.gz"
+      url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-darwin-amd64.tar.gz"
       sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Syfra3/syfra/releases/download/ancora-v#{version}/ancora-#{version}-linux-arm64.tar.gz"
+      url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-linux-arm64.tar.gz"
       sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
     else
-      url "https://github.com/Syfra3/syfra/releases/download/ancora-v#{version}/ancora-#{version}-linux-amd64.tar.gz"
+      url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-linux-amd64.tar.gz"
       sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
     end
   end
