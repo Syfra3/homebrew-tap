ae24b069b0f5a0a1ed4e2df3ef62511fa6504aaa
Matias Celiz <matias.celizm@gmail.com>
2026-04-06 20:43:02 -0500

    chore: update ancora formula to v1.0.0 with SHA256 checksums
    - Updated SHA256 checksums for all platforms
    - darwin-amd64: 80f3f8cf649a9d52a458874984ae525955c74e124c1fbc4db8f69fad1ab513bf
    - darwin-arm64: 4fe55e282aba1ae9d470d3614e865df165df394bd73b827a0dad6b9f7449357f
    - linux-amd64: 761f91594a2e997e072b0632e0c398908df2ca03b44e09db4b8fad62c306efc8
    - linux-arm64: b2095f32d37a2c1d99992e05c859e45095fcc8ef6972c2b52c67a2104ce344cf

--- Formula/ancora.rb
+++ Formula/ancora.rb
@@ -7,20 +7,20 @@ class Ancora < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
+      sha256 "4fe55e282aba1ae9d470d3614e865df165df394bd73b827a0dad6b9f7449357f"
     else
       url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
+      sha256 "80f3f8cf649a9d52a458874984ae525955c74e124c1fbc4db8f69fad1ab513bf"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      sha256 "b2095f32d37a2c1d99992e05c859e45095fcc8ef6972c2b52c67a2104ce344cf"
     else
       url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_AMD64"
+      sha256 "761f91594a2e997e072b0632e0c398908df2ca03b44e09db4b8fad62c306efc8"
     end
   end
 
