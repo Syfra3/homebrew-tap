bb1d0bb1bf0c9aa2ecdcefeceac5d03887099228
Matias Celiz <matias.celizm@gmail.com>
2026-04-09 18:36:18 -0500

    chore: update ancora to v1.2.1

--- Formula/ancora.rb
+++ Formula/ancora.rb
@@ -1,26 +1,26 @@
 class Ancora < Formula
   desc "Persistent memory system for AI agents with hybrid search and MCP integration"
   homepage "https://github.com/Syfra3/ancora"
-  version "1.1.0"
+  version "1.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-darwin-arm64.tar.gz"
-      sha256 "482240abc44d9d52654a80286fb793e01de1e83c52030e17ceabff027f804250"
+      sha256 "921908b3896bf8852f8a62ff8c7ed99ff899a5fff60b5be73d5d4205f81ae23b"
     else
       url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-darwin-amd64.tar.gz"
-      sha256 "4cbe7ec865baaa7393d018ce49c40f2d2ba2454615bafd5d48c825638b64b9b9"
+      sha256 "f22c07947b09bcd539a51ff7575be1e00f57074eb4e7032df6bcaf9ae6785ff9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-linux-arm64.tar.gz"
-      sha256 "19d5a9df07dd585d4eeffa8cbdaa579796594eef4a2e466a14714efcaf50ae2d"
+      sha256 "bcffa4a77ee292c9656aefb7a9c68145cb5b45d6b94d9e311e1b5af5644ffb2c"
     else
       url "https://github.com/Syfra3/ancora/releases/download/v#{version}/ancora-#{version}-linux-amd64.tar.gz"
-      sha256 "1dc2eee133e4209ad9509a782c80486148d081d9713dbbbddeefe7deeed1f824"
+      sha256 "4a98d52efd551736e56f3f0c345d78b4a01b800a50f60409c57c90dc5d9d797a"
     end
   end
 
